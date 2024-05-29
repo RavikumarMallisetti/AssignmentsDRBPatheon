fa5d8a363377e88f6e21754843ea1167804e2567
Mallisetti Kumar <mallisetti.kumar@globallogic.com>
2024-05-29 13:45:27 +0530

    change

--- SampleTests.cs
+++ SampleTests.cs
@@ -14,25 +14,15 @@ namespace AtataSampleProject
 
             LoginAndNavigateToProductsPage().ProductsButton.ClickAndGo<ProductsPage>()
               .PageTitle.Should.Contain("Products - Atata Sample App");
-            var xx=AtataContext.Current.Driver.FindElements(By.XPath("//table/descendant::tr/td"));
-            var numberOfRows = xx.Count() / 4;
-            for (var i = 0; i < numberOfRows; i++)
-            {
-                var name = xx[i].Text;
-                var price = xx[i+1].Text;
-                var amount = xx[i + 2].Text;
 
-                i=i+4;
-            }
-
-            //LoginAndNavigateToProductsPage().ProductsButton.ClickAndGo<ProductsPage>()
-            //  .PageTitle.Should.Contain("Products - Atata Sample App")
-            //   .ProductsTable.Rows.Count.Get(out int count)
-            //   .ProductsTable.Rows[x => x.Name == "Armchair"].DeleteUsingJQueryConfirmViaTrigger()
-            //   .ProductsTable.Rows[x => x.Name == "Armchair"].Should.Not.BePresent()
-            //   .ProductsTable.Rows.Should.HaveCount(count - 1)
-            //   .ProductsTable.Rows.ForEach(x=>x.Amount).v
-            //   .ProductsTable.Rows.Select(x => x.Price.Value).Value.Sum().Equals("945");
+            LoginAndNavigateToProductsPage().ProductsButton.ClickAndGo<ProductsPage>()
+              .PageTitle.Should.Contain("Products - Atata Sample App")
+               .ProductsTable.Rows.Count.Get(out int count)
+               .ProductsTable.Rows[x => x.Name == "Armchair"].DeleteUsingJQueryConfirmViaTrigger()
+               .ProductsTable.Rows[x => x.Name == "Armchair"].Should.Not.BePresent()
+               .ProductsTable.Rows.Should.HaveCount(count - 1);
+               //.ProductsTable.Rows.ForEach(x => x.Amount).v
+               //.ProductsTable.Rows.Select(x => x.Price.Value).Value.Sum().Equals("945");
 
         }
 
